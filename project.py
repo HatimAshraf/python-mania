f196972a21ece65d0d35cf8cdf7d0d024225c4cc
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-02-21 02:25:02 +0100

    introduce questions for my Trivia game

---
+++ project.py
@@ -0,0 +1,17 @@
+#prepare questions to ask
+#save the answer to the questions
+#make sure questions will be pop randomly
+#ask the questions
+#check if the answer is correct
+#keep track of the score
+#display the score
+
+questions = {
+  "What is the capital of France?": "Paris",
+  "How many planets in the solar system?": "8",
+  "What is the largest mammal?": "Blue Whale",
+  "What is the largest ocean?": "Pacific Ocean",
+  "Which country won 2002 FIFA World Cup?": "Brazil",
+  "who has won the most ATP titles in Mens Singles?":"Djocovic",
+  "what is 2*2?":"4"
+}
